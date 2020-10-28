acff930b13498aaac99d423dc3844de04b1e7089
48910376 <Meddeb.Jonas@gmail.com>
2020-10-28 23:53:45 +0100

    Changed structure and worked on method to read data and convert each img to a vector.

--- Model/DataProcessing.py
+++ Model/DataProcessing.py
@@ -1,10 +1,29 @@
 import pandas as pd
 from skimage import io
+import os
+import ImageProcessing as ip
 
 
 def read_data(path):
     ''' reads images from file'''
+
+    for file in os.listdir(path):
+        
+        #Identify subject and image detail
+        name = file.split(".")
+        subject = name[0]
+        atribute = name[1]
+
+        #Specifying path
+        file_path = os.path.join(path,file)
+
+        #Reading image
+        img = io.imread(file_path, as_gray=True)
+
+        #Creating a vector
+        v = ip.img2vector(img, axis=1)
+
+        print(v)
     return None
 
-
-
+read_data("..\Data\Images")
--- Model/ImageProcessing.py
+++ Model/ImageProcessing.py
@@ -1,11 +1,15 @@
-import matplotlib.pyplot as plt
-from skimage import data
-from skimage import color
+import numpy as np
 
 
-def img2vector(img):
+def img2vector(img, axis = 0):
     '''converts img to vector'''
-    return None 
+    length = img.shape[0]*img.shape[1]
+    if (axis == 0):
+        return np.reshape(img, (length, 1), order="C")
+    elif (axis == 1):
+        return np.reshape(img, (length, 1), order="F")
+    else:
+        return None 
 
 def img2gray(img):
     '''convert img to a black and white img'''
