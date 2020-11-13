e18f11b0678349768f39f1e30ab417907df61425
Jonas Meddeb <48910376+ShaggyBommba@users.noreply.github.com>
2020-11-13 23:06:31 +0100

    Version Two
    Version Two

--- Src/DataProcessing.py
+++ Src/DataProcessing.py
@@ -1,4 +1,6 @@
-import pandas as pd
+from typing import Tuple
+import numpy as np
+import random
 from skimage import io
 import os
 import sys
@@ -8,7 +10,9 @@ import ImageProcessing as ip
 def read_data(path):
     ''' reads images from file'''
     print("reading images from file")
-    data = dict()
+    X = []
+    Y = []
+
     for file in os.listdir(path):
 
         #Identify subject and image detail
@@ -24,17 +28,38 @@ def read_data(path):
         img = io.imread(file_path, as_gray=True)
 
         #Creating a vector
-        v = ip.img2vector(img, axis=1)
+        v = ip.img2vector(img, axis=0)
 
-        #adding to dictionary 
-        data[(subject,atribute)] = v
+        #adding to X and Y 
+        X.append(v)
+        Y.append((subject,atribute))
 
     #creating a matrix consisting of each image as a columnvector
-    df = pd.DataFrame(data) 
-    return df.T
+    X = np.array(X)
+    Y = np.array(Y)
+
+    return X,Y
+
+
+def split(X, Y, p=0.20, seed=None):
+    random.seed(seed)
+
+    idx_train = []
+    idx_test = []
+
+    bins = np.split([ i for i in range(len(Y[:,0] )) ], np.unique(Y[:,0], return_index=True)[1][1:])
+    for bin in bins:
+        random.shuffle(bin)
+        n = int( p * len(bin) )
+        idx_test = idx_test + list(bin[:n])
+        idx_train = idx_train + list(bin[n:])
+
+
+    return X[idx_train,:], Y[idx_train,:], X[idx_test,:], Y[idx_test,:]
+
 
 def standardization(data):
     ''' standardizing data so mean is zero '''
-    mean = data.mean(axis=0)
+    mean = data.mean(axis=1,keepdims=True)
     data = data -mean
     return data
--- Src/Main.py
+++ Src/Main.py
@@ -1,19 +1,54 @@
 from Model import PCA
+import numpy as np
 import DataProcessing as dp
+import matplotlib.pyplot as plt
 
 
 
-pca = PCA()
-load = input("enter 1 to load data")
+def test(X, Y, k, r=10, weighted=False):
+    result = np.empty(r)
+    for i in range(r):
+        X_train, Y_train, X_test, Y_test = dp.split(X,Y,p=0.20)
+        pca = PCA(X_train,Y_train)
+        pca.fit()
+        predictions = pca.predict(X_test,k, weighted=weighted)      
 
-if(load == "1"):
-    pca.load()
+        score = (predictions == Y_test[:,0])
+        score = score.sum()/len(score)
+        result[i] = score
+        print("Score: {}".format(score))
+    return result.mean()
 
-else:
-    data = dp.read_data("C:\\Users\\Jonas Meddeb\\Desktop\\Jonas Meddeb\\Datascience\\PCA---Image-recognition-\\Data\Images")
-    data = dp.standardization(data)
-    data2 = pca.fit(data)
-    pca.save()
+def plot(pca):
+    pca.plot_Eigenfaces(5,5)
+    pca.plot_pricipla_axis(2,2)
+    pca.plot_eigenvalues()
+
+def test_alogorithm(X, Y, r=10, weighted=False):
+    result_unweighted = []
+    result_weighted = [] 
+    for i in range(1,r):
+        print(i)
+        result_unweighted.append(test(X,Y,i,weighted=False))
+        result_weighted.append(test(X,Y,i,weighted=True))
+    plt.plot(result_unweighted, label = "unweighted")
+    plt.plot(result_weighted, label = "weighted")
+    plt.legend()
+    plt.show()
+
+
+
+X,Y = dp.read_data("C:\\Users\\Jonas Meddeb\\Desktop\\Jonas Meddeb\\Datascience\\PCA---Image-recognition-\\Data\Images")
+X_train, Y_train, X_test, Y_test = dp.split(X,Y,p=0.20)
+test_alogorithm(X,Y)
+#pca = PCA(X_train,Y_train)
+#pca.fit()
+#test(X,Y,5,weighted=False)
+#plot(pca)
+
+
+
+
 
 
 
